3234cfdc3f2a00b85f7bd192c7aa3bf8030ecfb3
Youngjin <leeyjin212@gmail.com>
2024-04-05 11:09:03 +0800

    profile setup view added

--- FoodieConnect/src/router/index.js
+++ FoodieConnect/src/router/index.js
@@ -1,33 +1,39 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Login from '@/components/Login.vue';
+import Login from "@/components/Login.vue";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
+import ProfileView from "@/views/ProfileView.vue";
 // import Profile from '@/views/Profile.vue';
 
 const routes = [
   {
-    path: '/', // The root path should only be declared once.
-    name: 'Login',
-    component: Login
+    path: "/", // The root path should only be declared once.
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/home',
-    name: 'Home', // Assuming you want to navigate to Home component via '/home' path
-    component: Home
+    path: "/home",
+    name: "Home", // Assuming you want to navigate to Home component via '/home' path
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: ProfileView,
   },
   // {
   //   path: '/profile',
   //   name: 'Profile',
   //   component: Profile
   // }
-]
+];
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
-export default router
+  routes,
+});
+export default router;
