ea250f97d6bee5230a8edcb358a1c5dc0feb36c2
Youngjin <leeyjin212@gmail.com>
2024-04-08 15:31:08 +0800

    profile editing view edited

--- FoodieConnect/src/router/index.js
+++ FoodieConnect/src/router/index.js
@@ -1,14 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "@/components/Login.vue";
-import About from "@/views/About.vue";
+import Logout from "../components/Logout.vue";
 import Home from "@/views/Homepage.vue";
+import About from "@/views/About.vue";
+import Homepage from "@/views/Homepage.vue";
 import ProfileView from "@/views/ProfileView.vue";
+import UpdateProfile from "@/views/UpdateProfile.vue";
 
 const routes = [
   {
     path: "/", // The root path should only be declared once.
     name: "Login",
     component: Login,
+  },
+  {
+    path: "/logout",
+    name: "Logout",
+    component: Logout,
   },
   {
     path: "/home",
@@ -23,7 +31,12 @@ const routes = [
   {
     path: "/profile",
     name: "Profile",
-    component: Profile,
+    component: ProfileView,
+  },
+  {
+    path: "/update-profile", // Add this route for UpdateProfile
+    name: "UpdateProfile",
+    component: UpdateProfile,
   },
 ];
 const router = createRouter({
